51142c6967d39d518d6f44ca12f0eb047afba89f
Diego_Barreto <diebarreto5@gmail.com>
2024-06-19 17:06:11 -0300

    CRIAÇão dos get e set do usuarioDTO

--- dradoramas/dradoramas/src/DTO/UsuarioDTO.java
+++ dradoramas/dradoramas/src/DTO/UsuarioDTO.java
@@ -3,5 +3,85 @@ package DTO;
 
 
 public class UsuarioDTO {
-    //se fude diego
+    private int ID;
+    private String Nome;
+    private int Idade;
+    private String Genero;
+    private String Email;
+    private String Senha;
+
+    
+    
+    
+    
+    public int getID() {
+        return ID;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public int getIdade() {
+        return Idade;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public void setIdade(int Idade) {
+        this.Idade = Idade;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 43 * hash + this.ID;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final UsuarioDTO other = (UsuarioDTO) obj;
+        return this.ID == other.ID;
+    }
 }
