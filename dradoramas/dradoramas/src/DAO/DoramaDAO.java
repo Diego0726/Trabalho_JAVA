db0fd768d474d3891f82c3615b40c2aac562355a
Diego_Barreto <diebarreto5@gmail.com>
2024-06-19 21:02:31 -0300

    BAGULHANDO O BAGULHO

--- dradoramas/dradoramas/src/DAO/DoramaDAO.java
+++ dradoramas/dradoramas/src/DAO/DoramaDAO.java
@@ -1,13 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
-/**
- *
- * @author Pichau
- */
+
+import DTO.DoramaDTO;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import DTO.UsuarioDTO;
+import javax.swing.JOptionPane;
+import java.sql.PreparedStatement;
+
+
 public class DoramaDAO {
+    
+    Connection conn;
+    PreparedStatement pstm;
+    
+    public void cadastrarDorama(UsuarioDTO objusuariodto) {
+        String sql = "insert into usuario (plataforma, nome, genero, temp, eps,observacao) values (?,?,?,?,?,?)";
+        conn = new ConexaoDAO().conectaBD();
+          
+        try {
+            pstm = conn.prepareStatement(sql);
+            pstm.setString(1, objusuariodto.getplataforma());
+            pstm.setString(2, objusuariodto.getnome());
+            pstm.setString(3, objusuariodto.getgenero());
+            pstm.setString(4, objusuariodto.gettemp());
+            pstm.setString(5, objusuariodto.geteps());
+            pstm.setString(6, objusuariodto.getobservacao());
+            
+            
+            pstm.execute();
+            pstm.close();
+         
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "UsuarioDAO: " + erro);
+        }
+        
+        public ResultSet autenticacaoUsuario(DoramaDTO objusuariodto) {
+        conn = new ConexaoDAO().conectaBD();
+
+        try {
+            String sql = "select * from usuario where plataforma = ? and nome = ? and genero = ? and temp = ? and eps = ? and observacao ";
+            PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.setString(1, objusuariodto.getplataforma());
+            pstm.setString(2, objusuariodto.getnome());
+            pstm.setString(3, objusuariodto.getgenero());
+            pstm.setString(4, objusuariodto.gettemp());
+            pstm.setString(5, objusuariodto.geteps());
+            pstm.setString(6, objusuariodto.getobservacao());
+
+            ResultSet rs = pstm.executeQuery();
+            return rs;
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "JOptionPaneUsuarioDAO: " + erro);
+            return null;
+        }
+    }
+    
+    
+    
+    
     
 }
