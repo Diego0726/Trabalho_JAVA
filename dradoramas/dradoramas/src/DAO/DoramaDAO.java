751d899b90bca8529353410665d321dcc1008ba9
Gisele Leite <egslie.lie@gmail.com>
2024-06-21 16:51:10 -0300

    sobre o DORAMA DAO

--- dradoramas/dradoramas/src/DAO/DoramaDAO.java
+++ dradoramas/dradoramas/src/DAO/DoramaDAO.java
@@ -1,20 +1,33 @@
 package DAO;
 
+<<<<<<< HEAD
 import DTO.DoramaDTO;
+=======
+
+>>>>>>> 3d1190d2f44cb9a11528778a14f6254e7c0951a9
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.ResultSet;
-import DTO.UsuarioDTO;
+import DTO.DoramaDTO;
 import javax.swing.JOptionPane;
 import java.sql.PreparedStatement;
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 3d1190d2f44cb9a11528778a14f6254e7c0951a9
 public class DoramaDAO {
     
     Connection conn;
     PreparedStatement pstm;
     
+<<<<<<< HEAD
     public void cadastrarDorama(UsuarioDTO objusuariodto) {
         String sql = "insert into usuario (plataforma, nome, genero, temp, eps, observacao) values (?,?,?,?,?,?)";
+=======
+    public void cadastrarDorama(DoramaDTO objusuariodto) {
+        String sql = "insert into usuario (plataforma, nome, genero, temp, eps,observacao) values (?,?,?,?,?,?)";
+>>>>>>> 3d1190d2f44cb9a11528778a14f6254e7c0951a9
         conn = new ConexaoDAO().conectaBD();
           
         try {
@@ -22,8 +35,8 @@ public class DoramaDAO {
             pstm.setString(1, objusuariodto.getplataforma());
             pstm.setString(2, objusuariodto.getnome());
             pstm.setString(3, objusuariodto.getgenero());
-            pstm.setString(4, objusuariodto.gettemp());
-            pstm.setString(5, objusuariodto.geteps());
+            pstm.setfloat(4, objusuariodto.gettemp());
+            pstm.setfloat(5, objusuariodto.geteps());
             pstm.setString(6, objusuariodto.getobservacao());
             
             pstm.execute();
@@ -33,17 +46,18 @@ public class DoramaDAO {
             JOptionPane.showMessageDialog(null, "UsuarioDAO: " + erro);
         }
         
-        public ResultSet autenticacaoUsuario(DoramaDTO objusuariodto) {
+        public ResultSet autenticacaDorama(DoramaDTO objusuariodto){
         conn = new ConexaoDAO().conectaBD();
 
         try {
             String sql = "select * from usuario where plataforma = ? and nome = ? and genero = ? and temp = ? and eps = ? and observacao ";
-            PreparedStatement pstm = conn.prepareStatement(sql);
-            pstm.setString(1, objusuariodto.getplataforma());
-            pstm.setString(2, objusuariodto.getnome());
-            pstm.setString(3, objusuariodto.getgenero());
-            pstm.setString(4, objusuariodto.gettemp());
-            pstm.setString(5, objusuariodto.geteps());
+            PreparedStatement pstm;
+            pstm = conn.prepareStatement(sql);
+            pstm.setString(1, objusuariodto.setplataforma());
+            pstm.setString(2, objusuariodto.settnome());
+            pstm.setString(3, objusuariodto.setgenero());
+            pstm.setString(4, objusuariodto.settemp());
+            pstm.setString(5, objusuariodto.seteps());
             pstm.setString(6, objusuariodto.getobservacao());
 
             ResultSet rs = pstm.executeQuery();
@@ -54,7 +68,8 @@ public class DoramaDAO {
             return null;
         }
     }
-    
+
+
     
     
     
